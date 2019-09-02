4fcad6f9eeec7d20685d5836a06e727292476505
jiangzheng24 <jiangzheng1@jd.com>
2019-09-02 16:01:57 +0800

    基础环境构建

---
+++ backend_utils/src/main/java/com/mooc/jiangzh/springcloud/baseutils/BackendUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.mooc.jiangzh.springcloud.baseutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUtilsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackendUtilsApplication.class, args);
+  }
+
+}
--- backend_zuul/src/main/java/com/mooc/jiangzh/springcloud/BackendZuulApplication.java
+++ backend_zuul/src/main/java/com/mooc/jiangzh/springcloud/BackendZuulApplication.java
@@ -2,8 +2,9 @@ package com.mooc.jiangzh.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-
+@EnableZuulProxy
 @SpringBootApplication
 public class BackendZuulApplication {
 
