9d15c2965fe5c90cf2a6fcc5131ad73b5b29ed1c
jiangzheng24 <jiangzheng1@jd.com>
2019-09-02 15:47:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# meetingfilm_backend
---
+++ backend_zuul/src/main/java/com/mooc/jiangzh/springcloud/BackendZuulApplication.java
@@ -0,0 +1,14 @@
+package com.mooc.jiangzh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BackendZuulApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackendZuulApplication.class, args);
+  }
+
+}
