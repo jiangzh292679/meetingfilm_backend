23b68b76cd11c95ae1a4c685f2e8e522cf888c32
jiangzheng24 <jiangzheng1@jd.com>
2019-09-20 12:50:02 +0800

    修改父工程Pom文件

---
+++ backend_cinema/src/main/java/com/mooc/jiangzh/springcloud/backend/cinema/BackendCinemaApplication.java
@@ -0,0 +1,14 @@
+package com.mooc.jiangzh.springcloud.backend.cinema;
+
+import com.mooc.jiangzh.springcloud.baseutils.utils.MD5Util;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCinemaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackendCinemaApplication.class, args);
+  }
+
+}
