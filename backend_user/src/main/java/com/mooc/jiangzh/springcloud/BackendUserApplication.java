e68fc55d49d0b0d836de39db37a6de61c6538057
jiangzheng24 <jiangzheng1@jd.com>
2019-12-05 20:03:16 +0800

    增加CinemaId支持

--- backend_user/src/main/java/com/mooc/jiangzh/springcloud/BackendUserApplication.java
+++ backend_user/src/main/java/com/mooc/jiangzh/springcloud/BackendUserApplication.java
@@ -3,6 +3,8 @@ package com.mooc.jiangzh.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @EnableDiscoveryClient
 @SpringBootApplication
